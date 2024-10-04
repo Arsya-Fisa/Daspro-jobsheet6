48c3ddbce283fd63747ca0a39b52ce95f4a157ea
Arsya-Fisa <Arsya Fikri S>
2024-10-04 16:19:34 +0700

    Modifikasi kedua

--- Pemilihan2Percobaan101.java
+++ Pemilihan2Percobaan101.java
@@ -11,9 +11,11 @@ public class Pemilihan2Percobaan101 {
         tahun = absen01.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) == 0 && (tahun % 400) ==0) {
-                  System.out.println("Tahun Kabisat ");
-                
+            if ((tahun % 100) == 0) {
+                  if ((tahun % 400) ==0) {
+                    System.out.println("Tahun Kabisat ");
+                  }else
+                  System.out.println("Bukan tahun kabisat");
             }else
             System.out.println("Bukan tahun kabisat");
         } else
